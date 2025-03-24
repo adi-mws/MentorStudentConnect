3f650d3b0283dd9688b07c599e8f246f0a9b63de
HHFC-Trust <hhfcavsar2020@gmail.com>
2025-03-24 17:53:33 +0530

    Dashboard Enhanced with the menu bar

--- client/src/components/layout/Sidebar/Sidebar.jsx
+++ client/src/components/layout/Sidebar/Sidebar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Sidebar.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-export default function Sidebar({}) {
+export default function Sidebar() {
+  const location = useLocation(); 
+  const [locationState, setLocationState] = useState('');
+  const routeTitles = {
+    '/dashboard': 'dashboard',
+    '/dashboard/profile': 'profile',
+    '/dashboard/goals': 'goals',
+    '/dashboard/mentors': 'mentors',
+    '/dashboard/students': 'students',
+    '/dashboard/messages': 'messages',
+  };
 
+  useEffect(() => {
+    setLocationState(routeTitles[location.pathname] || '');
+  }, [location.pathname])
+
+  
   return (
     <div className='Sidebar'>
         <ul className="sidebar-category head">
@@ -12,15 +27,15 @@ export default function Sidebar({}) {
             <span className="tag">Student</span>
         </ul>
         <ul className='sidebar-category '>
-            <li ><Link to="/dashboard" className='sidebar-item'>Dashboard</Link></li>
-            <li ><Link to="/dashboard/profile" className='sidebar-item'>Profile</Link></li>
-            <li ><Link to="/dashboard/mentors" className='sidebar-item'>Mentors</Link></li>
-            <li ><Link to="/dashboard/goals" className='sidebar-item'>Goals</Link></li>
+            <li ><Link to="/dashboard" className={`${locationState === 'dashboard' ? 'active': ''} sidebar-item`}><i class="fa-solid fa-chart-line"></i> Dashboard</Link></li>
+            <li ><Link to="/dashboard/profile" className={`${locationState === 'profile' ? 'active': ''} sidebar-item`}><i class="fa-solid fa-circle-user"></i> Profile</Link></li>
+            <li ><Link to="/dashboard/mentors" className={`${locationState === 'mentors' ? 'active': ''} sidebar-item`}><i class="fa-solid fa-user-tie"></i> Mentors</Link></li>
+            <li ><Link to="/dashboard/goals" className={`${locationState === 'goals' ? 'active': ''} sidebar-item`}><i class="fa-solid fa-bullseye"></i> Goals</Link></li>
         </ul>
         <ul className="sidebar-category">
             <h3 className='sidebar-category-item'>Others</h3>
-            <li><Link to="/dashboard/goals" className='sidebar-item'>Home</Link></li>
-            <li><Link to="/dashboard/about" className='sidebar-item'>About</Link></li>
+            <li><Link to="/" className='sidebar-item'>Home</Link></li>
+            <li><Link to="/about" className='sidebar-item'>About</Link></li>
         </ul>
     </div>
   )
